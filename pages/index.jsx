c6e09f63b6849eadeb6db43e17f2f21a87e0bc70
ThalesTrombim <thalestr@outlook.com>
2022-01-27 19:55:07 -0300

    adicionando o supabase

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,35 +3,6 @@ import { useState } from 'react';
 import appConfig from '../config.json';
 import Router from 'next/router';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
-
 function Titulo(props) {
   const Tag = props.tag || 'h1';
   return (
